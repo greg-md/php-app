68c037608d4e808979be83d4f88d8d46d1455699
Grigorii Duca <contact@greg.md>
2017-06-29 14:34:31 +0300

    .

--- app/Http/HttpBootstrap.php
+++ app/Http/HttpBootstrap.php
@@ -3,8 +3,6 @@
 namespace App\Http;
 
 use App\Resources\HttpRoutes;
-use DebugBar\JavascriptRenderer;
-use DebugBar\StandardDebugBar;
 use Greg\Framework\Http\BootstrapAbstract;
 use Greg\Support\Http\Request;
 use Greg\Support\Http\Response;
@@ -26,25 +24,6 @@ class HttpBootstrap extends BootstrapAbstract
     {
         $this->app()->listen(HttpKernel::EVENT_RUN, function () {
             $this->ioc()->load(HttpRoutes::class);
-        });
-    }
-
-    public function bootDebugBar()
-    {
-        $this->ioc()->inject(StandardDebugBar::class, function () {
-            return new StandardDebugBar();
-        });
-
-        $this->ioc()->inject(JavascriptRenderer::class, function (StandardDebugBar $bar) {
-            return $bar->getJavascriptRenderer('/debug');
-        });
-
-        $this->app()->listen(HttpKernel::EVENT_FINISHED, function (Response $response, JavascriptRenderer $renderer) {
-            if ($this->app()['debug'] and !Request::isAjax() and $response->isHtml()) {
-                $response->setContent(
-                    $response->getContent() . '<span></span>' . $renderer->renderHead() . $renderer->render()
-                );
-            }
         });
     }
 }
--- bootstrap/init.php
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env php
-<?php
-
-require_once __DIR__ . '/../vendor/autoload.php';
-
-\Greg\Support\Dir::copy(__DIR__ . '/../vendor/maximebf/debugbar/src/DebugBar/Resources', __DIR__ . '/../public/debug');
-
-echo 'Application initialized!' . PHP_EOL;
